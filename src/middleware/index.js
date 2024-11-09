fa0b93127f5e62bcbdd9255b0ff3e811bea4025a
Thomas Karanikiotis <tkaranikio@gmail.com>
2024-11-09 21:56:16 +0200

    Update index.js

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,6 @@
+// This function changes the default server timeout
 export const setServerTimeout = (millis = 5 * 60 * 1000) => (req, res, next) => {
-	req.setTimeout(millis, () => res.status(408).json({ message: "Request Timeout" }));
-	res.setTimeout(millis, () => res.status(503).json({ message: "Service Unavailable" }));
+	req.setTimeout(millis, () => res.status(408).json({ message: "Request Timeout" })); // Set timeout in request object
+	res.setTimeout(millis, () => res.status(503).json({ message: "Service Unavailable" })); // Set timeout in response object
 	next();
 };
